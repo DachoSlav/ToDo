fc72e1ffd217ee02be6acbd54fcb1f36a10a7417
DachoSlav <david.312kocevski@outlook.com>
2021-09-19 20:53:47 +0200

    draggable

--- main.js
+++ main.js
@@ -17,3 +17,34 @@ add__note.addEventListener("click", function(){
     }, 250)
 
  },);
+ var noteName = document.getElementById("form__subject");
+ var noteDescription = document.getElementById("form__description");
+ var noteDate = document.getElementById("form__duedate"); 
+ 
+ add.addEventListener("click", function(){
+    
+    // document.getElementById("todo__item").innerHTML = 
+    // noteName.value + " " 
+    // + noteDescription.value + " " 
+    // + noteDate.value;
+
+    const listElement = document.createElement("div");
+    const todo = document.getElementById("todo");
+    
+    listElement.className += "todo__item";
+    listElement.setAttribute("id","todo__item");
+    listElement.innerHTML = noteName.value;
+    todo.appendChild(listElement);
+   
+   
+
+
+    document.getElementById("display-none").style = 'display: none';
+
+    noteName.value = "";
+    noteDescription.value = "";
+    noteDate.value = "";
+
+ })
+
+ 
