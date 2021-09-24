eaa19063e179d1f77faa3f5680bb0f49fd5955bd
DachoSlav <david.312kocevski@outlook.com>
2021-09-24 22:39:36 +0200

    counters

--- main.js
+++ main.js
@@ -2,74 +2,69 @@ var add__note = document.getElementById("add__form");
 var add = document.getElementById("add");
 var discard = document.getElementById("discard");
 
+var notes__created__counter = 0;
+var notes__done__counter = 0;
 
+add__note.addEventListener("click", function () {
+  setTimeout(function () {
+    document.getElementById("display-none").style = "display: block";
+  }, 100);
+});
 
-add__note.addEventListener("click", function(){ 
-    setTimeout(function(){
-        document.getElementById("display-none").style = 'display: block';
-    }, 100)
-    
- });
+discard.addEventListener("click", function () {
+  setTimeout(function () {
+    document.getElementById("display-none").style = "display: none";
+  }, 250);
+});
+var noteName = document.getElementById("form__subject");
+var noteTime = document.getElementById("form__time");
+var noteDate = document.getElementById("form__duedate");
+var productivity = document.getElementById("success__rate");
 
- discard.addEventListener("click", function(){ 
-    setTimeout(function(){
-        document.getElementById("display-none").style = 'display: none';
-    }, 250)
-
- },);
- var noteName = document.getElementById("form__subject");
- var noteTime = document.getElementById("form__time");
- var noteDate = document.getElementById("form__duedate"); 
- 
- add.addEventListener("click", function(){
-    if(noteName.value == "" || parseInt(noteName.value)+0 == noteName.value){
-        alert("Please enter your Note Name properly");
-    }else{
-    
-    
-
-    
+add.addEventListener("click", function () {
+  if (noteName.value == "" || parseInt(noteName.value) + 0 == noteName.value) {
+    alert("Please enter your Note Name properly");
+  } else {
     const listElement = document.createElement("div");
     const todo = document.getElementById("todo");
-    const done = document.getElementById("done")
+    const done = document.getElementById("done");
     const doneButton = document.createElement("button");
-    
-
-
-
+    notes__created__counter++;
+    document.getElementById("notes__created").innerHTML =
+      "Notes Created: " + notes__created__counter;
 
     listElement.className += "todo__item";
-    listElement.setAttribute("id","todo__item");
-    listElement.innerHTML = noteName.value+"  -" + ' '+ " Due: "+ noteTime.value+"h" + " " + noteDate.value;
-    
-    const theName = listElement.innerHTML = noteName.value;
+    listElement.setAttribute("id", "todo__item");
+    listElement.innerHTML =
+      noteName.value +
+      "  -" +
+      " " +
+      " Due: " +
+      noteTime.value +
+      "h" +
+      " " +
+      noteDate.value;
+
+    const theName = (listElement.innerHTML = noteName.value);
     doneButton.setAttribute("id", "done__button");
     doneButton.textContent = "Done";
 
-
-    
     todo.appendChild(listElement);
     listElement.appendChild(doneButton);
 
+    doneButton.addEventListener("click", function () {
+      todo.removeChild(listElement);
+      listElement.removeChild(doneButton);
+      done.appendChild(listElement);
+      listElement.innerHTML = theName;
+      listElement.setAttribute("class", "done__style__element");
+      notes__done__counter++;
+      document.getElementById("notes__done").innerHTML = "Notes Done: " + notes__done__counter;
+      
+    });
 
-    doneButton.addEventListener("click",function(){
-        todo.removeChild(listElement);
-        listElement.removeChild(doneButton);
-        done.appendChild(listElement);
-        listElement.innerHTML = theName;
-        listElement.setAttribute("class", "done__style__element")
-        
-    } )
-   
- 
-    document.getElementById("display-none").style = 'display: none';
+    document.getElementById("display-none").style = "display: none";
     noteName.value = "";
     noteDate.value = "";
-
-
-}
- })
-
-
-
- 
+  }
+});
